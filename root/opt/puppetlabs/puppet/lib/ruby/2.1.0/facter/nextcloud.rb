5bcfbf724752d6ed0ed9ef384a24d4dff434a789
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2020-01-15 15:57:41 +0100

    facter: improve user count
    Count only users who logged at least once.
    Previously implementation was counting all users from LDAP.

--- root/opt/puppetlabs/puppet/lib/ruby/2.1.0/facter/nextcloud.rb
+++ root/opt/puppetlabs/puppet/lib/ruby/2.1.0/facter/nextcloud.rb
@@ -5,7 +5,8 @@ require 'rubygems'
 Facter.add('nextcloud') do
     setcode do
         nextcloud = {}
-        users = Facter::Core::Execution.exec("su - apache -s /bin/bash -c \"source /opt/rh/rh-php72/enable; cd /usr/share/nextcloud/; php occ user:list | wc -l \"")
+        pass = File.read("/var/lib/nethserver/secrets/nextcloud").strip
+        users = Facter::Core::Execution.exec("mysql -BN -u nextcloud -p#{pass} nextcloud -e \"select count(*) from oc_accounts where uid != 'admin'\"")
         nextcloud['users'] = users.to_i
         size = Facter::Core::Execution.exec("du -s /var/lib/nethserver/nextcloud/")
         nextcloud['size'] = size.to_i
