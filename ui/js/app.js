fc7b8d1cf14c4e60e7a608c79adc0528efeaca8a
Andrea Leardini <andrea.leardini@nethesis.it>
2020-06-17 16:01:33 +0200

    ui: display numbers in a human readable form
    NethServer/dev#6206

--- ui/js/app.js
+++ ui/js/app.js
@@ -46,3 +46,49 @@ $(document).on("nethserver-loaded", function() {
   });
   /* */
 });
+
+function humanFormat(number, decimals = false) {
+  var result;
+
+  switch (true) {
+    case number === null || number === "" || isNaN(number):
+      result = "-";
+      break;
+
+    case number >= 0 && number < 1000:
+      result = number;
+      break;
+
+    case number >= 1000 && number < Math.pow(1000, 2):
+      if (decimals) {
+        result = Math.round(number / 1000 * 10) / 10 + " K";
+      } else {
+        result = Math.round(number / 1000) + " K";
+      }
+      break;
+
+    case number >= Math.pow(1000, 2) && number < Math.pow(1000, 3):
+      if (decimals) {
+        result = Math.round(number / Math.pow(1000, 2) * 10) / 10 + " M";
+      } else {
+        result = Math.round(number / Math.pow(1000, 2)) + " M";
+      }
+      break;
+
+    case number >= Math.pow(1000, 3) && number < Math.pow(1000, 4):
+      if (decimals) {
+        result = Math.round(number / Math.pow(1000, 3) * 10) / 10 + " B";
+      } else {
+        result = Math.round(number / Math.pow(1000, 3)) + " B";
+      }
+      break;
+
+    default:
+      if (decimals) {
+        result = Math.round(number / Math.pow(1000, 4) * 10) / 10 + " T";
+      } else {
+        result = Math.round(number / Math.pow(1000, 4)) + " T";
+      }
+  }
+  return result;
+}
