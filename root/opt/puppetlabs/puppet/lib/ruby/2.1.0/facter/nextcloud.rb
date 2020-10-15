f970fb0ebd8ee44bfcd221d1f8132bbcd281f2be
Davide Principi <davide.principi@nethesis.it>
2020-10-15 15:02:15 +0200

    Read the admin user name from e-smith DB

--- root/opt/puppetlabs/puppet/lib/ruby/2.1.0/facter/nextcloud.rb
+++ root/opt/puppetlabs/puppet/lib/ruby/2.1.0/facter/nextcloud.rb
@@ -25,11 +25,16 @@ Facter.add('nextcloud') do
     setcode do
         users_count = 0
         begin
+            admin = Facter::Core::Execution.execute("/sbin/e-smith/config getprop admins user", :timeout => 10)
+        rescue Facter::Core::Execution::ExecutionFailure
+            admin = "admin"
+        end
+        begin
             # Count nextcloud users, excluding "admin", "admin@" and never-logged-in users
             users_list = Facter::Core::Execution.execute("occ user:list -i --output=json", :timeout => 30)
             JSON.parse(users_list).each do |uid, item|
                 next if uid == "admin"
-                next if item["email"][0..5] == "admin@"
+                next if item["email"][0..5] == admin + "@"
                 next if item["last_seen"] == "1970-01-01T00:00:00+00:00"
                 users_count += 1
             end
